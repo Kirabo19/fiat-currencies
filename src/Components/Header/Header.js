f1f4a20c9d9050ea2a5c8228962377d3608559ae
Kirabo19 <kapojames@gmail.com>
2023-01-13 21:07:10 +0300

    Base file setup Fetch Api

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaLessThan, FaMicrophone } from 'react-icons/fa';
+import { FiSettings } from 'react-icons/fi';
+import './Header.css';
+
+const Header = () => {
+  const iconStyle = {
+    color: 'rgb(212, 209, 209)',
+    cursor: 'pointer',
+    textDecoration: 'none',
+  };
+
+  return (
+    <Navbar>
+      <nav>
+        <ul className="navLinks">
+          <li>
+            <Link to="/" style={iconStyle}>
+              <FaLessThan className="lessthan" style={iconStyle} />
+              Home
+            </Link>
+          </li>
+          <li className="headerList">
+            <h1>Crypto Metrics</h1>
+          </li>
+          <li className="headerList">
+            <div className="icons-class">
+              <FaMicrophone style={iconStyle} />
+              <FiSettings style={iconStyle} />
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/Redux/Currency/CurrencySlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getCurrencies = createAsyncThunk(
+  'fetch/currencies',
+  async () => {
+    const response = await fetch('https://api.coinstats.app/public/v1/coins');
+
+    const data = await response.json();
+
+    const currencyData = data.coins.map((currency) => ({
+      id: currency.id,
+      img: currency.icon,
+      name: currency.name,
+      price: currency.price,
+      rank: currency.rank,
+      symbol: currency.symbol,
+      marketCap: currency.marketCap,
+      availableSupply: currency.availableSupply,
+      contractAddress: currency.contractAddress,
+      decimals: currency.decimals,
+      priceBtc: currency.priceBtc,
+      priceChange1d: currency.priceChange1d,
+      priceChange1h: currency.priceChange1h,
+      priceChange1w: currency.priceChange1w,
+      totalSupply: currency.totalSupply,
+      twitterUrl: currency.twitterUrl,
+      volume: currency.volume,
+      websiteUrl: currency.websiteUrl,
+      show: false,
+    }));
+    console.log(currencyData);
+    return currencyData;
+  },
+);
+
+const initialState = [];
+
+export const currencySlice = createSlice({
+  name: 'currency',
+  initialState,
+  reducers: {
+    setCurrencyReducer: (state, action) => state.map((currency) => {
+      if (currency.id === action.payload) {
+        return { ...currency, show: true };
+      }
+      return { ...currency, show: false };
+    }),
+  },
+  extraReducers: {
+    [getCurrencies.fulfilled]: (state, action) => action.payload,
+  },
+});
+
+export const { setCurrencyReducer } = currencySlice.actions;
+
+export default currencySlice.reducer;
